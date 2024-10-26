288636b9f1267dbb414b0bd09d0dc329309455b8
bonchondev <benzay212@gmail.com>
2024-10-25 21:11:53 -0400

    added solid query

--- views/src/layouts/Layout.jsx
+++ views/src/layouts/Layout.jsx
@@ -1,13 +1,21 @@
 import { Link } from 'inertia-adapter-solid'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/solid-query'
+
+const queryClient = new QueryClient();
 
 export default function Layout(props) {
   return (
-    <main>
-      <header class="flex flex-row p-3 gap-x-5">
-        <Link href="/" class="text-5xl font-bold underline">Home</Link>
-        <Link href="/about" class="text-5xl font-bold underline">About</Link>
-      </header>
-      <article class='p-3'>{props.children}</article>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main>
+        <header class="flex flex-row p-3 gap-x-5">
+          <Link href="/" class="text-5xl font-bold underline">Home</Link>
+          <Link href="/about" class="text-5xl font-bold underline">About</Link>
+        </header>
+        <article class='p-3'>{props.children}</article>
+      </main>
+    </QueryClientProvider>
   )
 }
--- views/src/pages/About.jsx
+++ views/src/pages/About.jsx
@@ -1,14 +1,33 @@
 import Layout from '../layouts/Layout'
 import { Title } from '@solidjs/meta'
+import { createQuery } from '@tanstack/solid-query'
+import ky from 'ky';
 
 export default function About(props) {
   const amount = () => props.amount;
+  const query = createQuery(() => ({
+    queryKey: ['data'],
+    queryFn: () => ky.post('/data').json()
+  }))
   return (
-    <Layout>
+    <>
       <Title>About</Title>
       <section>
         Amount : {amount()}
+        <Switch>
+          <Match when={query.isSuccess}>
+            <p>{query.data.dollars}</p>
+          </Match>
+          <Match when={query.isError}>
+            <p>Error: {query.error}</p>
+          </Match>
+          <Match when={query.isPending}>
+            <p>loading...</p>
+          </Match>
+        </Switch>
       </section>
-    </Layout>
+    </>
   )
 }
+
+About.layout = Layout
